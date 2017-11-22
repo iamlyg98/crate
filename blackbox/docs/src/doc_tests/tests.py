37c328084dd120818d326e254db0489239515ff2
Naomi Slater <nslater@tumbolia.org>
2017-11-22 17:33:34 +0100

    fixup! reshuffle general use docs

--- blackbox/docs/src/doc_tests/tests.py
+++ blackbox/docs/src/doc_tests/tests.py
@@ -433,7 +433,7 @@ def test_suite():
 
     # Documentation tests
     docs_suite = unittest.TestSuite()
-    s = docsuite('../../general/blob.txt', parser=bash_parser, setUp=setUp)
+    s = docsuite('../../general/blobs.txt', parser=bash_parser, setUp=setUp)
     s.layer = crate_layer
     docs_suite.addTest(s)
     for fn in ('protocols/http.txt',):
